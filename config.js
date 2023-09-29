ad5b393a4b9ef6e991142bb279034ae20884f89c
KPInc425 <kpinc425@gmail.com>
2023-09-29 13:39:41 -0700

    update config to work with docker

--- config.js
+++ config.js
@@ -1,26 +1,10 @@
 const fs = require('fs');
 
-var sendGridKey = '';
-
-fs.readFile('/creds/.env', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  sendGridKey = data;
-  console.log("stuff is here", sendGridKey);
-
-  module.exports = {
-    PORT: process.env.PORT || 3015,
-    SENDGRID_API_KEY: sendGridKey,
-    SOURCE_EMAIL: process.env.SOURCE_EMAIL,
-    DESTINATION_EMAILS: process.env.DESTINATION_EMAILS,
-  }
-});
+let sendGridKey = fs.readFileSync('/creds/.env', 'utf8').toString()
 
 module.exports = {
   PORT: process.env.PORT || 3015,
-  SENDGRID_API_KEY: "",
+  SENDGRID_API_KEY: sendGridKey,
   SOURCE_EMAIL: process.env.SOURCE_EMAIL,
   DESTINATION_EMAILS: process.env.DESTINATION_EMAILS,
 }
